144362e0d644872746f0f38a3b253e62bd4121a4
Ruslan Kozak <kozakrm@dtek.com>
2022-06-03 23:12:09 +0300

    draft 01

--- src/examples/Home.js
+++ src/examples/Home.js
@@ -1,7 +1,11 @@
-
-import React from "react";
+import React from 'react';
 
 function Home() {
-  return <h2>Home lala</h2>;
+  return (
+    <>
+      <h2>Home lala</h2>
+      <div>test rebase</div>
+    </>
+  );
 }
 export default Home;
